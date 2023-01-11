0973180fa9fa877b071f1725c10e4ddbe07b383d
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-11 23:08:49 +0100

    css added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import './Product/Product.css'
 
 const Product = ({ product }) => {
   return (
-    <div>
-      <img src={product.image} alt={product.title} />
-      <h3>{product.title}</h3>
-      <p>{product.description}</p>
+    <div className="product-container">
+      <span className="product-box">
+        <img src={product.image} alt={product.title} />
+        <h3>Title: {product.title}</h3>
+        <p>Description: {product.description}</p>
+        <p className="product-price">Price: {product.price}</p>
+      </span>
     </div>
   );
 };
 
 export default Product;
+
