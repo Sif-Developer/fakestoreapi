5c31ff5269da68ac9e94d73a112928191f8b175f
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-01-12 11:22:49 +0100

    fixed the bug of the pagination, now is working correctly

--- src/features/products/productsService.js
+++ src/features/products/productsService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "https://fakestoreapi.com";
 
 const getAllProducts = async () => {
-  const res = await axios.get(API_URL + "/products?limit=5");
+  const res = await axios.get(API_URL + "/products");
   return res.data;
 };
 const productsService = {
